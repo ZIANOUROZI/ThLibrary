9824f021528fb2f45132d3172476717aa88855a4
Ziano <Ziano@ZIA>
2023-11-10 13:50:30 +0100

    done base class

---
+++ Bok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThLibrary
+{
+    internal class Bok
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Isbn { get; set; }
+        public Bok(string title, string author, int isbn)
+        {
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title} Author: {Author} Isbn: {Isbn}");
+        }
+    }
+}
